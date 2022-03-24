bd2bd5a5aa90d80c3c58412319e2ef4d61db6ca3
sethblakley <sethblakley869@gmail.com>
2022-03-24 04:48:15 -0400

    frontend

---
+++ Frontend/devins-calc/src/App.js
@@ -0,0 +1,21 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <iframe
+          width="920"
+          height="920"
+          src="http://www.youtube.com/embed/oHg5SJYRHA0?autoplay=1&mute=1"
+          allow="autoplay"
+          frameborder="0"
+          allowfullscreen
+        ></iframe>
+      </header>
+    </div>
+  );
+}
+
+export default App;
